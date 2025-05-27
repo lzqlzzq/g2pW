21da044fb4309c08c9cdfa5b0ea748c71ccab674
Zhongqi Luo <luozhongqi@mail.com>
2025-05-27 17:06:24 +0800

    Fix gbk decoding error on windows

--- g2pw/api.py
+++ g2pw/api.py
@@ -68,7 +68,7 @@ class G2PWConverter:
         sess_options.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
         sess_options.intra_op_num_threads = 2
         self.session_g2pw =  onnxruntime.InferenceSession(os.path.join(model_dir, 'g2pw.onnx'), sess_options=sess_options)
-        
+
         self.config = load_config(os.path.join(model_dir, 'config.py'), use_default=True)
 
         self.num_workers = num_workers if num_workers else self.config.num_workers
@@ -80,101 +80,33 @@ class G2PWConverter:
 
         polyphonic_chars_path = os.path.join(model_dir, 'POLYPHONIC_CHARS.txt')
         monophonic_chars_path = os.path.join(model_dir, 'MONOPHONIC_CHARS.txt')
-        self.polyphonic_chars = [line.split('\t') for line in open(polyphonic_chars_path, 'r', encoding='utf-8').read().strip().split('\n')]
-        self.monophonic_chars = [line.split('\t') for line in open(monophonic_chars_path, 'r', encoding='utf-8').read().strip().split('\n')]
+        self.polyphonic_chars = [line.split('\t') for line in open(polyphonic_chars_path, encoding='utf8', errors='ignore').read().strip().split('\n')]
+        self.monophonic_chars = [line.split('\t') for line in open(monophonic_chars_path, encoding='utf8', errors='ignore').read().strip().split('\n')]
         self.labels, self.char2phonemes = get_char_phoneme_labels(self.polyphonic_chars) if self.config.use_char_phoneme else get_phoneme_labels(self.polyphonic_chars)
 
         self.chars = sorted(list(self.char2phonemes.keys()))
         self.pos_tags = TextDataset.POS_TAGS
 
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               'bopomofo_to_pinyin_wo_tune_dict.json'), 'r', encoding='utf-8') as fr:
-            self.bopomofo_convert_dict = json.load(fr)
+                               'bopomofo_to_pinyin_wo_tune_dict.json'), 'r', encoding='utf8', errors='ignore') as fr:
+            self.bopomofo_convert_dict = json.loads(fr.read())
         self.style_convert_func = {
             'bopomofo': lambda x: x,
             'pinyin': self._convert_bopomofo_to_pinyin,
         }[style]
 
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               'char_bopomofo_dict.json'), 'r', encoding='utf-8') as fr:
-            self.char_bopomofo_dict = json.load(fr)
+                               'char_bopomofo_dict.json'), 'r', encoding='utf8', errors='ignore') as fr:
+            self.char_bopomofo_dict = json.loads(fr.read())
 
         self.enable_non_tradional_chinese = enable_non_tradional_chinese
         if self.enable_non_tradional_chinese:
             self.s2t_dict = {}
             for line in open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                    'bert-base-chinese_s2t_dict.txt'), 'r', encoding='utf-8').read().strip().split('\n'):
+                    'bert-base-chinese_s2t_dict.txt'), 'r', encoding='utf8', errors='ignore').read().strip().split('\n'):
                 s_char, t_char = line.split('\t')
                 self.s2t_dict[s_char] = t_char
 
     def _convert_bopomofo_to_pinyin(self, bopomofo):
         tone = bopomofo[-1]
         assert tone in '12345'
-        component = self.bopomofo_convert_dict.get(bopomofo[:-1])
-        if component:
-            return component + tone
-        else:
-            print(f'Warning: "{bopomofo}" cannot convert to pinyin')
-            return None
-
-    def _convert_s2t(self, sentence):
-        return ''.join([self.s2t_dict.get(char, char) for char in sentence])
-
-    def __call__(self, sentences):
-        if isinstance(sentences, str):
-            sentences = [sentences]
-
-        if self.enable_non_tradional_chinese:
-            translated_sentences = []
-            for sent in sentences:
-                translated_sent = self._convert_s2t(sent)
-                assert len(translated_sent) == len(sent)
-                translated_sentences.append(translated_sent)
-            sentences = translated_sentences
-
-        texts, query_ids, sent_ids, partial_results = self._prepare_data(sentences)
-        if len(texts) == 0:
-            # sentences no polyphonic words
-            return partial_results
-
-        dataset = TextDataset(self.tokenizer, self.labels, self.char2phonemes, self.chars, texts, query_ids,
-                              use_mask=self.config.use_mask, use_char_phoneme=self.config.use_char_phoneme,
-                              window_size=self.config.window_size, for_train=False)
-
-        dataloader = DataLoader(
-            dataset=dataset,
-            batch_size=self.batch_size,
-            collate_fn=dataset.create_mini_batch,
-            num_workers=self.num_workers
-        )
-
-        preds, confidences = predict(self.session_g2pw, dataloader, self.labels, turnoff_tqdm=self.turnoff_tqdm)
-
-        if self.config.use_char_phoneme:
-            preds = [pred.split(' ')[1] for pred in preds]
-
-        results = partial_results
-        for sent_id, query_id, pred in zip(sent_ids, query_ids, preds):
-            results[sent_id][query_id] = self.style_convert_func(pred)
-
-        return results
-
-    def _prepare_data(self, sentences):
-        polyphonic_chars = set(self.chars)
-        monophonic_chars_dict = {
-            char: phoneme for char, phoneme in self.monophonic_chars
-        }
-        texts, query_ids, sent_ids, partial_results = [], [], [], []
-        for sent_id, sent in enumerate(sentences):
-            partial_result = [None] * len(sent)
-            for i, char in enumerate(sent):
-                if char in polyphonic_chars:
-                    texts.append(sent)
-                    query_ids.append(i)
-                    sent_ids.append(sent_id)
-                elif char in monophonic_chars_dict:
-                    partial_result[i] =  self.style_convert_func(monophonic_chars_dict[char])
-                elif char in self.char_bopomofo_dict:
-                    partial_result[i] =  self.style_convert_func(self.char_bopomofo_dict[char][0])
-            partial_results.append(partial_result)
-        return texts, query_ids, sent_ids, partial_results
